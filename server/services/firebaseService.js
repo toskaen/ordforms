1df6d5a864f7600f54ca6069f30e828ec387254c
toskaen <155739476+toskaen@users.noreply.github.com>
2025-06-23 19:54:45 +0100

    Allow running without Firebase private key

--- server/services/firebaseService.js
+++ server/services/firebaseService.js
@@ -1,23 +1,28 @@
 const admin = require('firebase-admin');
-let serviceAccount = undefined;
+
+let serviceAccount;
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } catch {
+    serviceAccount = undefined;
+  }
 } else {
   try {
     serviceAccount = require('../../firebase-adminsdk.json');
   } catch {
-    serviceAccount = {};
+    serviceAccount = undefined;
   }
 }
 
-if (serviceAccount && Object.keys(serviceAccount).length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: process.env.FIREBASE_BUCKET
-  });
-} else {
-  admin.initializeApp();
+const adminConfig = {};
+if (serviceAccount && serviceAccount.private_key && serviceAccount.client_email) {
+  adminConfig.credential = admin.credential.cert(serviceAccount);
 }
+if (process.env.FIREBASE_BUCKET) {
+  adminConfig.storageBucket = process.env.FIREBASE_BUCKET;
+}
+admin.initializeApp(adminConfig);
 
 const db = admin.firestore();
 let storage;
